e2f4a666a1877880cef48fd59d4954267d8bcee1
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-15 11:48:36 +0100

    Updating import for CV

--- cv-website/src/components/CV.jsx
+++ cv-website/src/components/CV.jsx
@@ -1,4 +1,5 @@
 import "./website-styles.css";
+import cv from "./files/cv1.pdf"
 
 const CV = () => {
     return (
@@ -6,7 +7,7 @@ const CV = () => {
         <h1>CV</h1>
         <p>Below is a copy of my CV for you to browse.<br /><br />If you do wish to get in touch, you can reach me using the contact page!</p>
         <br />
-        <embed src="./files/cv.pdf" type="application/pdf" width="100%" height="900px" style={{marginLeft: "250px"}} />
+        <embed src={cv} type="application/pdf" width="100%" height="900px" style={{marginLeft: "250px"}} />
         <br /><br />
     </div>
     )
