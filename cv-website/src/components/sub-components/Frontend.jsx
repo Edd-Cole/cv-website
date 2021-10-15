70f391f38622eb20d9df8ef82a3a4f0e2519f091
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-15 11:57:25 +0100

    Updating import link for image

--- cv-website/src/components/sub-components/Frontend.jsx
+++ cv-website/src/components/sub-components/Frontend.jsx
@@ -1,4 +1,5 @@
 import "../website-styles.css";
+import gameImage from "../files/images/2048_image.png";
 
 const Frontend = () => {
     return (
@@ -8,7 +9,7 @@ const Frontend = () => {
         <p>The app is built in React.js </p>
         <p>The repo can be found by following this <a href="https://github.com/Edd-Cole/Game-2048" target="_blank">link</a>.</p>
         <p>Want to give the game a play? Try it <a href="https://6167f351c6f53c0007526bb3--sleepy-mcclintock-e0b8bf.netlify.app/" target="_blank">here</a>!</p>
-        <img class="img" src="../files/images/Screenshot 2021-09-28 at 11-13-48 React App.png" />
+        <img class="img" src={gameImage} style={{height: "400px", width: "auto"}} />
         <br /><br />
     </div>
     )
