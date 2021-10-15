ac6da73a26d9641b75c5faef809d231e4a1039b4
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-15 11:47:56 +0100

    Updating import statements for music

--- cv-website/src/components/sub-components/Music.jsx
+++ cv-website/src/components/sub-components/Music.jsx
@@ -1,4 +1,8 @@
 import "../website-styles.css";
+import carried from "../audios/carried_away.mp3";
+import gone from "../audios/Gone.mp3";
+import laced from "../audios/laced_peach.mp3";
+import carriedAcoustic from "../audios/carried_away_acoustic.mp3";
 
 const Music = () => {
     return (
@@ -9,23 +13,23 @@ const Music = () => {
         </p>
         <h3>Carried Away</h3>
         <p>One of the first songs I ever wrote. This is everyone's favourite, and mine too. Upbeat, happy and very catchy. I still work on this song when inspiration hits. It's 7 years old!</p>
-        <audio src="../audios/Carried Away V2.mp3" controls></audio>
+        <audio src={carried} controls></audio>
         <br />
         <h3>Gone</h3>
         <p>One of my more recent songs. I was inspired to write this after hearing this type of structure in some songs. It's built as an entire crescendo up to the final chorus where it is really meant to leave the listener with chills.<br /><br />I'm sure
             you'll appreciate the static melody whilst the song evolves around it.
         </p>
-        <audio src="../audios/Gone.mp3" controls></audio>
+        <audio src={gone} controls></audio>
         <br />
         <h3>Laced Peach</h3>
         <p>I have written some quite melancholy and poignant songs, too. Laced Peach is a good example of that where the guitar rings lonely for most of the song. Laced Peach always hits me from the moment that moment the first guitar note plays.<br /><br />Personally,
             I think it is one of my more beautiful songs.
         </p>
-        <audio src="../audios/Laced Peach.mp3" controls></audio>
+        <audio src={laced} controls></audio>
         <br />
         <h3>Carried Away (Acoustic)</h3>
         <p>Carried Away is my favourite song for a reason. And, if you have listened to all 3 beforehand, I'm sure this will be a good way to end the music section.<br /><br />We can't leave on a sombre note, so enjoy the acoustic version of Carried Away!</p>
-        <audio src="../audios/Carried Away (Acoustic).mp3" controls></audio>
+        <audio src={carriedAcoustic} controls></audio>
         <br /><br />
     </div>
     )
